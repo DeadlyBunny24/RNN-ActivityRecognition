41d8d73e4a6bee3688553b3e5800118d76cd5c04
DeadlyBunny24 <bambole8@gmail.com>
2017-12-28 12:59:25 -0500

    Added a parameter to create multiple recurrent units, each with different time delays

--- model.py
+++ model.py
@@ -1,14 +1,25 @@
 import tensorflow as tf
 from tensorflow.python import debug as tf_debug
-from dataset import _parse_function
+from play_dataset import _parse_function
+import math
 
 batch_size = 20
-hidden_size = 400
+hidden_size = 250
 num_classes = 2
 learning_rate = 0.01
 padding_size = 300
-num_epochs = 100
+num_epochs = 10000
 feature_size = 100
+# Should start from 1 and be sorted in ascending order.
+# Repeated values won't be considered, because cells are in a dictionary.
+net_arch = [1,5,10,20,40]
+
+writer_filename = 'na_{h}_lr_{l:.0E}_hs_{e:03d}_bs_{s:03d}'.format(
+    h=net_arch,
+    l=learning_rate,
+    e=hidden_size,
+    s=batch_size
+)
 
 # This section uses the Dataset API to iterate through a tfRecords dataset.
 # It iterates the dataset and applies tensor transformations to prepare the input
@@ -38,7 +49,10 @@ with tf.name_scope('dataset'):
     next_element = iterator.get_next()
 
     input_reshape = tf.reshape(next_element[0],[batch_size,padding_size,feature_size])
-    input_model = tf.transpose(input_reshape,[1,0,2],name='model_input')
+    input_transpose = tf.transpose(input_reshape,[1,0,2],name='model_input')
+    # This line slices the first 75 elements of the 275 feature vector.
+    # This correspond to the position (x,y,z) of the 25 joints.
+    input_model = tf.slice(input_transpose,[0,0,0],[padding_size,batch_size,75])
     sequence_sizes = next_element[2]
     tile_labels = tf.tile(next_element[1],[padding_size])
     reshape_labels = tf.reshape(tile_labels,[padding_size,batch_size])
@@ -55,15 +69,28 @@ input_model_ = tf.unstack(input_model,axis=0)
 
 # The variable scope has an initializer parameter that allows to control the
 # initilization of the variables inside of the scope.
-with tf.variable_scope('RNN'):
-    cell = tf.contrib.rnn.BasicRNNCell(hidden_size)
+with tf.variable_scope('RNN', reuse=None):
 
-    state = tf.zeros([batch_size,hidden_size],dtype=tf.float64,name='state_init')
-
+    cell = {}
+    out = {}
     outputs = []
 
-    for batch in input_model_:
-        output, state = cell(batch,state)
+    # cell = {key:tf.contrib.rnn.BasicRNNCell(hidden_size) for key in net_arch}
+    for key in net_arch:
+        with tf.variable_scope('{}'.format(key)):
+            cell[key] = tf.contrib.rnn.BasicRNNCell(hidden_size)
+
+    state = {key:tf.zeros([batch_size,hidden_size],dtype=tf.float64) for key in net_arch}
+
+    for count,batch in enumerate(input_model_):
+        for key in net_arch:
+            if key==1:
+                out[key], state[key] = cell[key](batch,state[key],'RNN/{}'.format(key))
+            elif (count % key)==0:
+                out[key], state[key] = cell[key](out[previous_key],state[key],'RNN/{}'.format(key))
+            previous_key = key
+
+        output = tf.concat([out[key] for key in net_arch],axis=1)
         outputs.append(output)
 
 # ouputs has shape [sequence_length, batch_size, hidden_size]
@@ -87,19 +114,12 @@ train_step = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
 with tf.name_scope('accuracy'):
     mask_as_int = tf.cast(sequence_mask,tf.int64)
     predictions_labels = tf.argmax(logits,2)
-    # real_labels = labels
-    # correct_prediction = tf.equal(predictions_labels,real_labels)
-    # real_correct_prediction = tf.cast(correct_prediction,tf.int64)*mask_as_int
-    # accuracy = tf.reduce_sum(tf.cast(real_correct_prediction,tf.float64)) /\
-    #             tf.reduce_sum(tf.cast(mask_as_int,tf.float64))
-
-    # This line is meant to extract the local variables associated with the accuracy.
-    # Accordingly, this allows us to reset them when one batch is completed
-    pred_vars = tf.contrib.framework.get_variables('accuracy', collection=tf.GraphKeys.LOCAL_VARIABLES)
-
+    pred_vars = tf.contrib.framework.get_variables('accuracy',
+                                        collection=tf.GraphKeys.LOCAL_VARIABLES)
     _,accuracy = tf.metrics.accuracy(labels=labels,
                                      predictions=predictions_labels,
                                      weights=mask_as_int)
+
 
 # Summaries collected for tensorboard.
 with tf.name_scope('summaries'):
@@ -120,7 +140,7 @@ with tf.Session() as sess:
     train_iterator_handle = sess.run(train_iterator.string_handle())
     test_iterator_handle = sess.run(test_iterator.string_handle())
 
-    writer = tf.summary.FileWriter('./graphs/hs_{}/'.format(hidden_size), sess.graph)
+    writer = tf.summary.FileWriter('./graphs/{}/'.format(writer_filename), sess.graph)
 
     # This runs the debugger
     # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
@@ -130,12 +150,13 @@ with tf.Session() as sess:
     while True:
         try:
             global_step+=1
-            _,summary_train = sess.run([train_step,merge_during_train],
+            _,summary_train,loss_ = sess.run([train_step,merge_during_train,loss],
                                         feed_dict={handle:train_iterator_handle})
 
-            if global_step % 125 == 0:
-                print 'Step: {}'.format(global_step)
+            if global_step % 100 == 0:
+                print 'Step: {} Loss: {}'.format(global_step, loss_)
 
+            if global_step % 1000 == 0:
                 # This resets the local variables associatted with the acc counter
                 sess.run(pred_vars)
                 sess.run(test_iterator.initializer)
@@ -145,13 +166,13 @@ with tf.Session() as sess:
                     try:
                         summary_test = sess.run(acc_summary,
                                                 feed_dict={handle:test_iterator_handle})
-                    except tf.errors.InvalidArgumentError, tf.errors.OutOfRangeError:
+                    except (tf.errors.InvalidArgumentError, tf.errors.OutOfRangeError):
                         print 'End of validation dataset'
                         writer.add_summary(summary_train,global_step)
                         writer.add_summary(summary_test,global_step)
                         break
 
-        except tf.errors.InvalidArgumentError, tf.errors.OutOfRangeError:
+        except (tf.errors.InvalidArgumentError, tf.errors.OutOfRangeError):
             print 'End of training dataset'
             writer.close()
             break
